1f394e83d69af200f9b56f6eab98db34ea711859
allierosel <104614515+allierosel@users.noreply.github.com>
2023-02-18 13:44:16 -0700

    pinch

--- hexGame.py
+++ hexGame.py
@@ -1,16 +1,9 @@
 
-
-name = input("Enter your name: ")
-print("Hello, " + name)
-
-# (white, red, green, blue, chartreuse)
-buttons = ["White","Red", "Blue","Green", "Chartreuse"]
-
 # Question 1
 bool = 0
 
 while bool == 0:
-    first_q = input("Hex code 000000. Click the color:")
+    first_q = input("Hex code FFFFFF. Click the color:")
     if first_q == "White":
         bool = 1
         print("Great job! you got the first button!")
@@ -59,9 +52,21 @@ while bool == 0:
     first_q = input("Hex code DFFF00. Click the color:")
     if first_q == "Chartreuse":
         bool = 1
+        print("Great job! you got the fith button!")
+    else:
+        print("Oops wrong button. Try again.")
+
+# Question 6
+bool = 0
+
+while bool == 0:
+    first_q = input("Hex code A020F0. Click the color:")
+    if first_q == "Purple":
+        bool = 1
         print("Great job! you got the Last button!")
     else:
         print("Oops wrong button. Try again.")
+
 
 
 print("Congradulations! Hex the Hamster can get hone!")
